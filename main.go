5d5947b292e69af8a743d0d145451d92857573c7
tlenferink <tlenferink@quintor.nl>
2024-04-22 14:15:19 +0200

    Added artists

--- main.go
+++ main.go
@@ -6,24 +6,51 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
+type Artist struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Country string `json:"country"`
+}
+
+type Album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
+	Artist Artist  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Harvest", Artist: "Neil Young", Price: 20.50},
-	{ID: "2", Title: "In Rainbows", Artist: "Radiohead", Price: 22.50},
-	{ID: "3", Title: "Mutter", Artist: "Rammstein", Price: 23.50},
+var artists = []Artist{
+	{ID: "1", Name: "Rammstein", Country: "DE"},
+	{ID: "2", Name: "Neil Young", Country: "US"},
+	{ID: "3", Name: "Radiohead", Country: "UK"},
+}
+
+var albums = []Album{
+	{ID: "1", Title: "Harvest", Artist: artists[1], Price: 20.50},
+	{ID: "2", Title: "In Rainbows", Artist: artists[2], Price: 22.50},
+	{ID: "3", Title: "Mutter", Artist: artists[0], Price: 23.50},
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/artists", getArtists)
+	router.GET("/artists/:id", getArtist)
 
 	router.Run(":8080")
+}
+
+func getArtist(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+		}
+	}
+}
+
+func getArtists(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, artists)
 }
 
 func getAlbums(c *gin.Context) {
